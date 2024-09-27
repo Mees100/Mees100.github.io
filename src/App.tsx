596c22acd8aa0c79601b200c8b480415b9203b2a
Alina <alina100chuk@gmail.com>
2024-09-27 18:45:56 +0200

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,78 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+
+function Layout() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  let menuLinksClassName = styles.menuLinks;
+  if (isMenuOpen) {
+    menuLinksClassName += " " + styles.menuLinksOpen;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className={styles.header}>
+        <Container>
+          <nav>
+            <h2 className={styles.navHeader}>Front-End Developer</h2>
+            <div
+              className={styles.menuIcon}
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+            >
+              {isMenuOpen ? (
+                <div className={styles.menuClose}>X</div>
+              ) : (
+                <>
+                  <div className={styles.menuBar}></div>
+                  <div className={styles.menuBar}></div>
+                  <div className={styles.menuBar}></div>
+                </>
+              )}
+            </div>
+            <div className={menuLinksClassName}>
+              <Link
+                className={styles.linkStyles}
+                to="/"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Home
+              </Link>
+              <Link
+                className={styles.linkStyles}
+                to="/skills"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Skills
+              </Link>
+              <Link
+                className={styles.linkStyles}
+                to="/certificates"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Certicates
+              </Link>              
+              <Link
+                className={styles.linkStyles}
+                to="/about"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                About
+              </Link>
+              <Link
+                className={styles.linkStyles}
+                to="/Contact"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Contact
+              </Link>
+            </div>
+          </nav>
+        </Container>
+      </header>
     </>
   );
 }
 
-export default App;
+export default Layout;
